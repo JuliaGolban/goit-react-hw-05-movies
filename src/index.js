37886e6bf71d987e8e7fe72db56eea11cd5abbf9
JuliaGolban <julia.j.shcherban@gmail.com>
2022-11-23 17:40:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { App } from './components/App/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/goit-react-hw-05-movies/"> */}
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
+      {/* <BrowserRouter> */}
       <App />
       <GlobalStyle />
     </BrowserRouter>
