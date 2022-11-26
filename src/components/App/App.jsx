422e6f3ff2625970992b44b1abf7f36f31374a43
JuliaGolban <julia.j.shcherban@gmail.com>
2022-11-26 23:07:51 +0200

    restructured app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,19 @@
-import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { AppBar } from 'components/AppBar/AppBar';
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import { SharedLayout } from "components/SharedLayout/SharedLayout";
 
-const Home = lazy(() => import('../../pages/Home/Home'));
-const Movies = lazy(() => import('../../pages/Movies/Movies'));
-const MovieDetails = lazy(() => import('../MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('../Cast/Cast'));
-const Reviews = lazy(() => import('../Reviews/Reviews'));
+const Home = lazy(() => import("../../pages/Home/Home"));
+const Movies = lazy(() => import("../../pages/Movies/Movies"));
+const MovieDetails = lazy(() =>
+  import("../../pages/MovieDetails/MovieDetails")
+);
+const Cast = lazy(() => import("../Cast/Cast"));
+const Reviews = lazy(() => import("../Reviews/Reviews"));
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<AppBar />}>
+      <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
         <Route path="movies/:movieId" element={<MovieDetails />}>
--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,28 +1,21 @@
-import { Suspense } from 'react';
-import { BsHouseDoorFill, BsFilm } from 'react-icons/bs';
-import { Outlet } from 'react-router-dom';
-import { HeaderContainer, Header, Nav, Link } from './AppBar.styled';
+import { BsHouseDoorFill, BsFilm } from "react-icons/bs";
+import { HeaderContainer, Header, Nav, Link } from "./AppBar.styled";
 
 export const AppBar = () => {
   return (
-    <>
-      <Header id="header">
-        <HeaderContainer>
-          <Nav>
-            <Link to="/">
-              <BsHouseDoorFill size="17" />
-              Home
-            </Link>
-            <Link to="movies">
-              <BsFilm size="13" />
-              Movies
-            </Link>
-          </Nav>
-        </HeaderContainer>
-      </Header>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Outlet />
-      </Suspense>
-    </>
+    <Header id="header">
+      <HeaderContainer>
+        <Nav>
+          <Link to="/">
+            <BsHouseDoorFill size="17" />
+            Home
+          </Link>
+          <Link to="movies">
+            <BsFilm size="13" />
+            Movies
+          </Link>
+        </Nav>
+      </HeaderContainer>
+    </Header>
   );
 };
